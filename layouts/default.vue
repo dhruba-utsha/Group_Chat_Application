d8269b6e074c7e0d019f3cd61c34cebab1687668
dhruba-utsha <utshasen676@gmail.com>
2025-01-21 14:48:08 +0600

    message option added

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-      <header class="shadow-sm bg-sky-500">
+      <header class="shadow-sm bg-sky-500 sticky top-0">
         <nav class="container mx-auto p-4 flex justify-between items-center text-gray-100">
           <NuxtLink to="/" class="font-bold text-[30px]">Home</NuxtLink>
           <ul class="flex gap-10">
