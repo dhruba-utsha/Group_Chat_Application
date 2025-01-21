fff2aef75b529a1b339f35ece4d9b9e88990718a
dhruba-utsha <utshasen676@gmail.com>
2025-01-21 15:27:23 +0600

    homePage updated

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,4 @@
 <template>
-    <h1 class="text-3xl font-bold text-green-500 underline">
-      Hello This is home page.
-    </h1>
     <div class="flex justify-between items-center py-[5rem]">
         <div>
             <h1 class="text-[50px] font-bold"> Let's Connect With <br> Group Chat Application  </h1>
