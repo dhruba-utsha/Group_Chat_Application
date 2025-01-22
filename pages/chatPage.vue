78827ea370e53b9f41d0c4b0e5bd48f56126ca5c
ziaul-hassan <ziaulhasanf@gmail.com>
2025-01-22 14:55:53 +0600

    correct the design in chat

--- pages/chatPage.vue
+++ pages/chatPage.vue
@@ -6,19 +6,25 @@
 
                 <div v-if="message.role === 'member'" class="text-white my-4 flex justify-start">
                     <div class="bg-gray-300 px-4 py-2 rounded-xl w-[40%]">
-                        <p class="font-bold text-[20px] text-sky-700">
-                            {{ getUserName(message.id) }}
+                        <p class="font-bold text-[20px] py-3 text-sky-700">
+                            
+                            <span class="text-white bg-gray-400 p-3 rounded-[50%]">{{ getFirstLetter(getUserName(message.id)) }}</span>
+                            {{ getUserName(message.id) }} <span class="text-gray-600 pl-[18rem] text-[12px]">    {{ formatDate(message.date) }}</span>
+
                         </p>
-                        <p class="text-[18px] text-black">{{ message.message }}</p>
+                        <p class="text-[18px] text-black pl-12 pb-3">{{ message.message }}</p>
+                        
                     </div>
                 </div>
 
                 <div v-else-if="message.role === 'admin'" class="text-white my-4 flex justify-end">
                     <div class="bg-gray-300 px-4 py-2 rounded-xl w-[40%]">
-                        <p class="font-bold text-[20px] text-green-700">
-                            {{ getUserName(message.id) }}
+                        <p class="font-bold text-[20px] py-3 text-green-700">
+                            <span class="text-white bg-gray-400 p-3 rounded-[50%]">{{ getFirstLetter(getUserName(message.id)) }}</span>
+                            {{ getUserName(message.id) }} <span class="text-gray-600 pl-[18rem] text-[12px]">    {{ formatDate(message.date) }}</span>
                         </p>
-                        <p class="text-[18px] text-black">{{ message.message }}</p>
+                        <p class="text-[18px] text-black pl-12 pb-3">{{ message.message }}</p>
+                       
                     </div>
                 </div>
             </div>
@@ -38,8 +44,6 @@
                 class="bg-blue-800 p-[0.5rem] px-[2rem] rounded-lg text-[20px] text-gray-100 font-semibold hover:bg-blue-600">
                 Send
             </button>
-
-
         </div>
     </div>
 </template>
@@ -58,12 +62,10 @@ export default {
         };
     },
     mounted() {
-
         const storedUsers = localStorage.getItem("users");
         if (storedUsers) {
             this.users = JSON.parse(storedUsers);
         }
-
 
         const storedMessages = localStorage.getItem("messages");
         if (storedMessages) {
@@ -73,34 +75,36 @@ export default {
     methods: {
         sendMessage() {
             if (this.newMessage.message && this.newMessage.id) {
-
                 const selectedUser = this.users.find((user) => user.id === this.newMessage.id);
-
                 if (!selectedUser) return;
-
 
                 const newMessage = {
                     message: this.newMessage.message,
                     role: selectedUser.role.toLowerCase(),
                     id: this.newMessage.id,
+                    date: new Date().toISOString(), 
                 };
 
-
                 this.messages.push(newMessage);
-
-
                 localStorage.setItem("messages", JSON.stringify(this.messages));
-
 
                 this.newMessage.message = "";
                 this.newMessage.id = "";
             }
         },
-
-
         getUserName(userId) {
             const user = this.users.find((user) => user.id === userId);
             return user ? user.name : "Unknown User";
+        },
+        getFirstLetter(userId) {
+            const user = this.users.find((user) => user.id === userId);
+            const twoletter= userId.slice(0,2);
+
+            return twoletter.toUpperCase();
+        },
+        formatDate(dateString) {
+            const date = new Date(dateString);
+            return date.toLocaleString(); 
         },
     },
 };
