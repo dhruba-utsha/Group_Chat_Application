b99c8174aa5a0c6022a6e8e75355a4bfddf0f71c
dhruba-utsha <utshasen676@gmail.com>
2025-01-22 15:30:07 +0600

    designed chat thread

--- pages/chatPage.vue
+++ pages/chatPage.vue
@@ -6,12 +6,14 @@
 
                 <div v-if="message.role === 'member'" class="text-white my-4 flex justify-start">
                     <div class="bg-gray-300 px-4 py-2 rounded-xl w-[40%]">
-                        <p class="font-bold text-[20px] py-3 text-sky-700">
-                            
-                            <span class="text-white bg-gray-400 p-3 rounded-[50%]">{{ getFirstLetter(getUserName(message.id)) }}</span>
-                            {{ getUserName(message.id) }} <span class="text-gray-600 pl-[18rem] text-[12px]">    {{ formatDate(message.date) }}</span>
+                        <div class="font-bold text-[20px] py-3 text-sky-700 flex items-center justify-between">
+                            <p>
+                            <span class="text-white bg-gray-600 p-3 rounded-[50%]">{{ getFirstLetter(getUserName(message.id)) }}</span>
+                            {{ getUserName(message.id) }}
+                            </p>
 
-                        </p>
+                            <p class="text-gray-600 text-[12px]">{{ formatDate(message.date) }}</p>
+                        </div>
                         <p class="text-[18px] text-black pl-12 pb-3">{{ message.message }}</p>
                         
                     </div>
@@ -19,10 +21,14 @@
 
                 <div v-else-if="message.role === 'admin'" class="text-white my-4 flex justify-end">
                     <div class="bg-gray-300 px-4 py-2 rounded-xl w-[40%]">
-                        <p class="font-bold text-[20px] py-3 text-green-700">
-                            <span class="text-white bg-gray-400 p-3 rounded-[50%]">{{ getFirstLetter(getUserName(message.id)) }}</span>
-                            {{ getUserName(message.id) }} <span class="text-gray-600 pl-[18rem] text-[12px]">    {{ formatDate(message.date) }}</span>
-                        </p>
+                        <div class="font-bold text-[20px] py-3 text-green-700 flex items-center justify-between">
+                            <p>
+                            <span class="text-white bg-gray-600 p-3 rounded-[50%]">{{ getFirstLetter(getUserName(message.id)) }}</span>
+                            {{ getUserName(message.id) }}
+                            </p>
+
+                            <p class="text-gray-600 pl-[10rem] text-[12px]">{{ formatDate(message.date) }}</p>
+                        </div>
                         <p class="text-[18px] text-black pl-12 pb-3">{{ message.message }}</p>
                        
                     </div>
