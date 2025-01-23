62b89ad4d4e2f86459633fb29ee236d61c538ecd
dhruba-utsha <utshasen676@gmail.com>
2025-01-23 15:24:53 +0600

    image in chat added

--- pages/userPage.vue
+++ pages/userPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-row">
    
-    <div class="bg-white text-white p-4 flex-1 w-[55%] h-[600px] overflow-scroll">
+    <div class="bg-white text-white p-4 flex-1 w-[55%] h-[700px] overflow-auto">
       <h1 class="font-bold mb-4 text-center text-[30px] text-black">User List</h1>
       <ul class="space-y-4">
         <li v-for="user in users" :key="user.id" class="p-5 bg-sky-500 rounded-md flex justify-between items-center">
